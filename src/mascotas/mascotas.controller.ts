68e9cf8cb94d07568733424f0a010cc9a7645e2e
Serena <rotondaserena3@gmail.com>
2025-10-21 15:25:06 -0300

    se agrego Get de registro de mascotas y Get por Id

--- src/mascotas/mascotas.controller.ts
+++ src/mascotas/mascotas.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { MascotasService } from './mascotas.service';
 
-@Controller('mascotas')
-export class MascotasController {}
+@Controller('registro')
+export class MascotasController {
+  constructor(private readonly mascotasService: MascotasService) {}
+  @Get('')
+  getListarMascotas() {
+    return this.mascotasService.getMascotas();
+  }
+  @Get(':id')
+  getListarPorId(@Param('id') idMascota: Number) {
+    return this.mascotasService.getMacotasId(idMascota);
+  }
+}
--- src/mascotas/mascotas.service.ts
+++ src/mascotas/mascotas.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MascotasService {}
+export class MascotasService {
+  registroDeMascotas: any = [];
+  constructor() {
+    let mascota = {
+      id: '1',
+      nombre: 'Firulais',
+      raza: 'caniche toy',
+      edad: '3',
+      vacunas: '0',
+      tratamiento: 'antibioticos',
+      diagnostico: 'Infeccion Urinaria',
+      duenio: 'Juan Perez',
+      telefono: '555-1234',
+    };
+    this.registroDeMascotas.push(mascota);
+    mascota = {
+      id: '2',
+      nombre: 'Fauto',
+      raza: 'caniche toy',
+      edad: '9',
+      vacunas: '4',
+      tratamiento: 'antibioticos',
+      diagnostico: 'Infeccion de piel',
+      duenio: 'Raul Sanchez',
+      telefono: '535-1294',
+    };
+    this.registroDeMascotas.push(mascota);
+    mascota = {
+      id: '3',
+      nombre: 'Lola',
+      raza: 'Labrador Retriever',
+      edad: '2',
+      vacunas: '3',
+      tratamiento: 'ninguno',
+      diagnostico: 'revision anual',
+      duenio: 'Julia Martinez',
+      telefono: '555-1354',
+    };
+    this.registroDeMascotas.push(mascota);
+  }
+  getMascotas() {
+    return this.registroDeMascotas;
+  }
+  getMacotasId(id: Number) {
+    for (let i = 0; i < this.registroDeMascotas.length; i++) {
+      if (this.registroDeMascotas[i].id == id) {
+        return this.registroDeMascotas[i];
+      }
+    }
+    return 'No se encontro el paciente';
+  }
+}
