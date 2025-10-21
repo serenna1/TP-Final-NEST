b202d0a70968331f1a0cb306942faa8a56b5116c
Serena <rotondaserena3@gmail.com>
2025-10-21 16:38:18 -0300

    se agrego Post para poder agendar turnos, en el modulo administracion

--- src/administracion/administracion.controller.ts
+++ src/administracion/administracion.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AdministracionService } from './administracion.service';
 
 @Controller('administracion')
-export class AdministracionController {}
+export class AdministracionController {
+  constructor(private readonly admiService: AdministracionService) {}
+  @Get('turnos')
+  getListarTurnos() {
+    return this.admiService.getTurnos();
+  }
+  @Post('nuevoturno')
+  postNuevosTurnos(@Body() nuevaTurno: any) {
+    return this.admiService.postAgendarTurno(nuevaTurno);
+  }
+}
--- src/administracion/administracion.service.ts
+++ src/administracion/administracion.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AdministracionService {}
+export class AdministracionService {
+  agendaDeTurnos: any = [];
+  constructor() {
+    let turno = {
+      telefono: '555-1234', //voy a usar el telefono como si fuese el id
+      duenio: 'Juan Perez',
+      nombreMascota: 'Firulais',
+      fecha: '20-11-25',
+      hora: '16:00',
+    };
+    this.agendaDeTurnos.push(turno);
+    turno = {
+      telefono: '535-1294', //voy a usar el telefono como si fuese el id
+      duenio: 'Raul Sanchez',
+      nombreMascota: 'Fauto',
+      fecha: '20-11-25',
+      hora: '16:30',
+    };
+    this.agendaDeTurnos.push(turno);
+  }
+
+  getTurnos() {
+    return this.agendaDeTurnos;
+  }
+  postAgendarTurno(nuevoTurno: any) {
+    if (!nuevoTurno || !nuevoTurno.fecha || !nuevoTurno.duenio) {
+      return 'Se ingresó un valor inválido';
+    }
+    this.agendaDeTurnos.push(nuevoTurno);
+    return `Se agendo el truno de ${nuevoTurno.duenio}`;
+  }
+}
